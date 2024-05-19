5199a26193193059606ee82b2eb825d174fc9bc4
Jeremy Schur <jeremy.schur02@gmail.com>
2024-05-18 19:54:59 -0600

    mostly fixed docker

--- client/src/resources/pages/App.js
+++ client/src/resources/pages/App.js
@@ -2,29 +2,26 @@ import React, {useEffect, useState} from 'react';
 
 function App() {
 
-  const [backendData, setBackendData] = useState([{}]);
+  const [testData, setTest] = useState([{}]);
 
   useEffect(() => {
-    fetch('/api').then(
-      response => response.json()
-    ).then(
-      data => {
-        setBackendData(data.users)
-      }
-    )
+    fetch('/api')
+      .then((response) => response.json())
+      .then((data) => setTest(data))
+      .catch((error) => console.log(error))
   }, []);
 
   return (
     <div>
-      {(typeof backendData.users === 'undefined') ? (
+      {(typeof testData.users === 'undefined') ? (
         <p>Loading...</p>
       ): (
-        backendData.users.map((user, index) => (
+        testData.users.map((user, index) => (
           <p key={index}>{user}</p>
         ))
       )}
     </div>
   );
 }
-
+ 
 export default App;
