c5bbd11eb65c3e649c1e42f828d043a73266d71c
Jeremy Schur <jeremy.schur02@gmail.com>
2024-05-25 13:26:32 -0600

    added router for different pages

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+
+import Home from './resources/pages/Home'
+import Profile from './resources/pages/Profile'
+
+export default function App() {
+
+  return (
+    <Router> 
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  )
+}
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './resources/pages/App.tsx'
-import Home from './resources/pages/Home.tsx'
+import App from './App.tsx'
 
-import './resources/css/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* <App /> */}
-    <Home />
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 )
