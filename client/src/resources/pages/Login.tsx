7800be9ab9dfb59565487a4490167fe30725ec27
Jeremy Schur <jeremy.schur02@gmail.com>
2024-05-25 23:20:15 -0600

    styling changed to login page

--- client/src/resources/pages/Login.tsx
+++ client/src/resources/pages/Login.tsx
@@ -31,32 +31,47 @@ export default function Login() {
     }
 
     return (
-        <form 
-            onSubmit={handleLogin}
-            className="flex flex-col w-1/4 mx-auto mt-20 p-4 bg-gray-200 rounded-lg shadow-lg"
-        >  
-            <input
-                className="mb-2 p-2 rounded-md" 
-                type="text" 
-                placeholder="username" 
-                name="username"
-                value={username} 
-                onChange={(e) => setUsername(e.target.value)} 
-            />
-            <input 
-                className="mb-2 p-2 rounded-md"
-                type="password" 
-                placeholder="password"
-                name="password" 
-                value={password} 
-                onChange={(e) => setPassword(e.target.value)} 
-            />
-            <button 
-                type="submit"
-                className="p-2 bg-blue-500 text-white rounded-md hover:bg-blue-700"
+        <div className="flex h-screen pt-32 justify-center">
+            <div className="mt-20 mr-10">
+                <h1 className="text-3xl mr-auto">ride share</h1>
+                <h2 className="text-xl mr-auto">For all your travel needs</h2>
+            </div>
+            <div 
+                style={{backgroundImage: "url(https://newyorkrentalbyowner.com/blog/wp-content/uploads/2019/09/Skiing-in-Upstate-New-York-10-Best-Ski-Resorts-For-Your-Winter-Vacation.jpeg)"}}
+                className="flex items-center justify-center w-1/5 h-4/6 bg-cover bg-center bg-no-repeat rounded-lg shadow-lg"
             >
-                Login
-            </button>
-        </form>
+                <form 
+                    onSubmit={handleLogin}
+                    className="flex flex-col p-4 w-full overflow-hidden"
+                >  
+                    <label htmlFor="username" className="text-white">Username</label>
+                    <input
+                        className="mb-3 p-2 rounded-md" 
+                        type="text" 
+                        id="username"
+                        placeholder="username" 
+                        name="username"
+                        value={username} 
+                        onChange={(e) => setUsername(e.target.value)} 
+                    />
+                    <label htmlFor="password" className="text-white">Username</label>
+                    <input 
+                        className="p-2 rounded-md"
+                        type="password" 
+                        id="password"
+                        placeholder="password"
+                        name="password" 
+                        value={password} 
+                        onChange={(e) => setPassword(e.target.value)} 
+                    />
+                    <button 
+                        type="submit"
+                        className="m-4 p-2 bg-blue-500 text-white rounded-md hover:bg-blue-700"
+                    >
+                        Login
+                    </button>
+                </form>
+            </div>
+        </div>
     )
 }
